c7bee19a779e6a067f40b58cdbc87309c81f8155
Rahmeh Daniel <danielr.97@hotmail.fr>
2022-02-03 11:13:10 +0100

    Données des structures complètes + début des commentaires sur fichiers

--- php/pages/header/header.php
+++ php/pages/header/header.php
@@ -1,30 +1,48 @@
+<!-- Header des pages en général -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <!-- Intégration de bootstrap -->
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
    rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+   <!-- Intégration de jquery -->
    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.css">
-   <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
+
+   <!-- Intégration de TableFilter librairie jquery permmettant d'intégrer de tableau filtrable et triable -->
+   <script src="../../TableFilter-master/dist/tablefilter/tablefilter.js"></script>
+
+   <!-- Intégration du CSS -->
    <link rel="stylesheet" href="../../css/style.css" type="text/css" />
    <link rel="stylesheet" href="../../css/responsive.css" type="text/css" />
+   
    <title>Document</title>
 </head>
+
 <body>
+   <!-- Partie header des pages -->
    <header>
+
+      <!-- Logo principal -->
       <a href="principal.php" id="banniere">
          <img src="../../img/logo/logo_img2.png" alt="Retour à l'accueil">
          <h3>Gestionnaire de cellules</h3>
       </a>
-         <form action="" id="search_bar_header">
-            <input type="text" name="search" placeholder="Entrer votre recherche" class="search_bar" required>
-            <button type="submit" name="btnEnvoiForm" title="Search" class="search_button"><img src="../../img/icon/search.png" class="icon_search" /></button>
-         </form>
+
+      <!-- Bar de recherche -->
+      <form action="" id="search_bar_header">
+         <input type="text" name="search" placeholder="Entrer votre recherche" class="search_bar" required>
+         <button type="submit" name="btnEnvoiForm" title="Search" class="search_button"><img src="../../img/icon/search.png" class="icon_search" /></button>
+      </form>
+
+      <!-- Boutons de navigation -->
       <nav>
          <?php if ($_SESSION['admin'] == 1) {?>
             <a href="settings_admin.php"  id="settings">
--- php/pages/header/settings_admin_header.php
+++ php/pages/header/settings_admin_header.php
@@ -1,3 +1,5 @@
+<!-- Header utilisé sur la page settings_admin.php afin de rendre actif le bouton settings_admin -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,26 +7,43 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta content="width=device-width, initial-scale=1" name="viewport" />
+
+   <!-- Intégration de bootstrap -->
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
    rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+   <!-- Intégration de jquery -->
    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+   
+   <!-- Intégration de TableFilter librairie jquery permmettant d'intégrer de tableau filtrable et triable -->
    <script src="../../TableFilter-master/dist/tablefilter/tablefilter.js"></script>
+
+   <!-- Intégration du CSS -->
    <link rel="stylesheet" href="../../css/style.css" type="text/css" />
    <link rel="stylesheet" href="../../css/responsive.css" type="text/css" />
+   
    <title>Paramètres admin</title>
 </head>
+
 <body>
+   <!-- Partie header des pages -->
    <header>
+
+      <!-- Logo principal -->
       <a href="principal.php" id="banniere">
          <img src="../../img/logo/logo_img2.png" alt="Retour à l'accueil">
          <h3>Gestionnaire de cellules</h3>
       </a>
-         <form action="" id="search_bar_header">
-            <input type="text" name="search" placeholder="Entrer votre recherche" class="search_bar" required>
-            <button type="submit" name="btnEnvoiForm" title="Search" class="search_button"><img src="../../img/icon/search.png" class="icon_search" /></button>
-         </form>
+
+      <!-- Bar de recherche -->
+      <form action="" id="search_bar_header">
+         <input type="text" name="search" placeholder="Entrer votre recherche" class="search_bar" required>
+         <button type="submit" name="btnEnvoiForm" title="Search" class="search_button"><img src="../../img/icon/search.png" class="icon_search" /></button>
+      </form>
+
+      <!-- Boutons de navigation -->
       <nav>
          <?php if ($_SESSION['admin'] == 1) {?>
             <a href="settings_admin.php"  id="settings_active">
